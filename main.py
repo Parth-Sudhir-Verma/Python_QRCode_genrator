5d78adaa495fd42011434f2aea77a9936266015b
Parth-Sudhir-Verma <parth.verma9332@gmail.com>
2025-10-31 19:56:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,4 +90,3 @@ async def verify_user(user_id: str):
             <p>Direct Link: <a href="{verify_url}" target="_blank">{verify_url}</a></p>
         """)
     return HTMLResponse("<h2>User not found</h2>")
-oihoihoi
